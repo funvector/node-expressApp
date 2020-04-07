41b8d09c71dc12a72fd45e7e0bdf631a1de71bd5
Vlad K <huza007@mail.ru>
2020-04-08 02:53:54 +0300

    feat: add session and auth

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const path = require('path')
+const csrf = require('csurf')
+const flash = require('connect-flash')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
+const MongoStore = require('connect-mongodb-session')(session)
 const homeRoutes = require('./routes/home')
 const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
 const ordersRoutes = require('./routes/orders')
 const coursesRoutes = require('./routes/courses')
 const authRoutes = require('./routes/auth')
-const User = require('./models/user')
+const varMiddleware = require('./middleware/variables')
+const userMiddleware = require('./middleware/user')
 
+const MONGODB_URI = `mongodb+srv://Vlad:nlfkha4b1e8@cluster0-qd2gg.mongodb.net/shop`
 const app = express()
 
 const hbs = exphbs.create({
@@ -17,22 +23,27 @@ const hbs = exphbs.create({
   extname: 'hbs'
 })
 
+const store = new MongoStore({
+  collection: 'sessions',
+  uri: MONGODB_URI
+})
+
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
-app.use(async (req, res, next) => {
-  try {
-    const user = await User.findById('5e81de06140dd41a6cfb0819')
-    req.user = user
-    next()
-  } catch (e) {
-    console.log(e)
-  }
-})
-
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
+app.use(session({
+  secret: 'some secret value',
+  resave: false,
+  saveUninitialized: false,
+  store
+}))
+app.use(csrf())
+app.use(flash())
+app.use(varMiddleware)
+app.use(userMiddleware)
 
 app.use('/', homeRoutes)
 app.use('/add', addRoutes)
@@ -45,21 +56,11 @@ const PORT = process.env.PORT || 3000
 
 async function start() {
   try {
-    const url = `mongodb+srv://Vlad:nlfkha4b1e8@cluster0-qd2gg.mongodb.net/shop`
-    await mongoose.connect(url, {
+    await mongoose.connect(MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false
     })
-    const candidate = await User.findOne()
-    if (!candidate) {
-      const user = new User({
-        email: 'vlad@mail.ru',
-        name: 'Vlad',
-        cart: {items: []}
-      })
-      await user.save()
-    }
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`)
     })
